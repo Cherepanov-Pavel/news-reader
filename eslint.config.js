a11380e9c47c2a3511d55b759a133668745c3c0a
Pavel <cherepanov.pavel.98@gmail.com>
2026-08-09 15:25:34 +0500

    chore: install and setup shareable-config lib

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import {
+  globalConfig,
+  jsConfig,
+  tsConfig,
+  vueConfig,
+} from '@cherepanov.pavel/shareable-config/tools/eslint-config/index.js';
+
+const eslintConfig = [
+  ...globalConfig,
+  jsConfig,
+  tsConfig,
+  vueConfig,
+];
+
+export function override() {
+  // you can override some part of config here, by eslintConfig.push()
+  // for the example, uncomment this line:
+  // eslintConfig.push({
+  //   files: ['**/*.js'],
+  //   rules: {
+  //     'no-dupe-keys': 'off',
+  //   },
+  // });
+}
+override();
+
+export default eslintConfig;
+
+// add later
+// {
+// 		plugins: {
+// 			json,
+// 		},
+// 	},
+// 	{
+// 		files: ["**/*.json"],
+// 		ignores: ["package-lock.json"],
+// 		language: "json/json",
+// 		rules: jsonRules
+// 	},
+// 	{
+// 		files: ["**/*.jsonc", ".vscode/*.json"],
+// 		language: "json/jsonc",
+// 		rules: jsonRules
+// 	},
+// 	{
+// 		files: ["**/*.json5"],
+// 		language: "json/json5",
+// 		rules: jsonRules
+// 	},
